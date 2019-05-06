54f40cdb19e12f968c877bff0725c73c108296f4
Oscar Valdez <oscar@ozipi.dev>
2019-05-05 20:29:54 -0500

    [v.02] add favicon

--- components/Meta.js
+++ components/Meta.js
@@ -5,6 +5,15 @@ const Meta = () => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <link rel="shortcut icon" href="/static/favicon.png" />
+
+    <link rel="apple-touch-icon" sizes="76x76" href="/static/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+    <link rel="manifest" href="/static/site.webmanifest" />
+    <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#472aa0" />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff" />
+
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
     <title>Ozipi::</title>
   </Head>
